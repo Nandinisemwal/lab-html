838f8d66631c2b25366e7d34e1c60c1b6a50d51c
Nandinisemwal <nandini.semwal@edu.sait.ca>
2024-11-17 15:36:56 -0700

    Add alert to javascript

--- js/main.js
+++ js/main.js
@@ -1,8 +1,16 @@
 /* 
-Add current year to footer
+Add current year to the footer
 */
+
 document.addEventListener('DOMContentLoaded', () => {
     const currentYear = new Date().getFullYear();
     const copyright = document.getElementById('copyright');
     copyright.textContent = `(c) ${currentYear}, Nandini Semwal`;
+})
+
+//add alert button to display Hello, world!
+
+const alertButton = document.getElementByIdElement("btn-alert");
+alertButton.addEventListener("click", function () {
+    alert("Hey What's up!");
 })

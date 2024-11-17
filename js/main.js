14feea3fe26ee4063a664d3ac36ecb9e6a79ad54
Nandinisemwal <nandini.semwal@edu.sait.ca>
2024-11-17 04:29:46 -0700

    ?? very confusing

--- js/main.js
+++ js/main.js
@@ -3,5 +3,6 @@ Add current year to footer
 */
 document.addEventListener('DOMContentLoaded', () => {
     const currentYear = new Date().getFullYear();
-    document.getElementById('copyright').textContent = `(c) ${currentYear}, Nandini Semwal`;
-});
+    const copyright = document.getElementById('copyright');
+    copyright.textContent = `(c) ${currentYear}, Nandini Semwal`;
+})

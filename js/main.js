8d0c187dbd6b79c76555e1da73e576c6ac3917d0
Nandinisemwal <nandini.semwal@edu.sait.ca>
2024-11-17 21:07:50 -0700

    fixing copyright mark

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ Add current year to the footer
 document.addEventListener('DOMContentLoaded', () => {
     const currentYear = new Date().getFullYear();
     const copyright = document.getElementById('copyright');
-    copyright.textContent = `(c) ${currentYear}, Nandini Semwal`;
+    copyright.textContent = ` ${currentYear}, Nandini Semwal`;
 })
 
 //add alert button to display Hello, world!
